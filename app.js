2cb1440560ccbc4a0e5713c40242e86b3b0767c8
sangbinyoo <ysbin0406@gmail.com>
2019-06-09 23:41:31 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/snake.html';
+      var t = 'timestone';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + url);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.listen(3000);
